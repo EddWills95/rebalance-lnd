a897ddec994d8bc6a46cbfc5bc2a6daca803adf2
EddWills95 <edd.williams@me.com>
2021-10-05 20:33:04 +0100

    linting

--- logic.py
+++ logic.py
@@ -46,10 +46,13 @@ class Logic:
         last_hop_alias_formatted = ""
         first_channel_id = 0
         if self.first_hop_channel:
-            first_hop_alias_formatted = format_alias(self.lnd.get_node_alias(self.first_hop_channel.remote_pubkey))
-            first_channel_id = format_channel_id(self.first_hop_channel.chan_id)
+            first_hop_alias_formatted = format_alias(
+                self.lnd.get_node_alias(self.first_hop_channel.remote_pubkey))
+            first_channel_id = format_channel_id(
+                self.first_hop_channel.chan_id)
         if self.last_hop_channel:
-            last_hop_alias_formatted = format_alias(self.lnd.get_node_alias(self.last_hop_channel.remote_pubkey))
+            last_hop_alias_formatted = format_alias(
+                self.lnd.get_node_alias(self.last_hop_channel.remote_pubkey))
         amount_formatted = format_amount(self.amount)
         if self.first_hop_channel and self.last_hop_channel:
             self.output.print_line(
@@ -57,7 +60,8 @@ class Logic:
                 f"back through {last_hop_alias_formatted}."
             )
         elif self.last_hop_channel:
-            self.output.print_line(f"Sending {amount_formatted} satoshis back through {last_hop_alias_formatted}.")
+            self.output.print_line(
+                f"Sending {amount_formatted} satoshis back through {last_hop_alias_formatted}.")
         else:
             self.output.print_line(
                 f"Sending {self.amount:,} satoshis from channel {first_channel_id} with {first_hop_alias_formatted}."
@@ -67,9 +71,12 @@ class Logic:
         payment_request = self.generate_invoice()
         min_fee_last_hop = None
         if self.first_hop_channel:
-            fee_rate_first_hop = self.lnd.get_ppm_to(self.first_hop_channel.chan_id)
-            policy_first_hop = self.lnd.get_policy_to(self.first_hop_channel.chan_id)
-            min_fee_last_hop = self.compute_fee(self.amount, fee_rate_first_hop, policy_first_hop)
+            fee_rate_first_hop = self.lnd.get_ppm_to(
+                self.first_hop_channel.chan_id)
+            policy_first_hop = self.lnd.get_policy_to(
+                self.first_hop_channel.chan_id)
+            min_fee_last_hop = self.compute_fee(
+                self.amount, fee_rate_first_hop, policy_first_hop)
         routes = Routes(
             self.lnd,
             payment_request,
@@ -79,13 +86,15 @@ class Logic:
             self.output
         )
 
-        self.initialize_ignored_channels(routes, fee_limit_msat, min_fee_last_hop)
+        self.initialize_ignored_channels(
+            routes, fee_limit_msat, min_fee_last_hop)
 
         tried_routes = []
         while routes.has_next():
             route = routes.get_next()
 
-            success = self.try_route(payment_request, route, routes, tried_routes)
+            success = self.try_route(
+                payment_request, route, routes, tried_routes)
             if success:
                 return True
         self.output.print_line("Could not find any suitable route")
@@ -99,18 +108,21 @@ class Logic:
             if self.reckless:
                 fee_limit_msat = self.fee_ppm_limit * self.amount / 1_000
             else:
-                fee_limit_msat = max(1_000, self.fee_ppm_limit * self.amount / 1_000)
+                fee_limit_msat = max(
+                    1_000, self.fee_ppm_limit * self.amount / 1_000)
         elif self.last_hop_channel:
             fee_rate = self.lnd.get_ppm_to(self.last_hop_channel.chan_id)
             if fee_rate > MAX_FEE_RATE:
-                last_hop_alias = self.lnd.get_node_alias(self.last_hop_channel.remote_pubkey)
+                last_hop_alias = self.lnd.get_node_alias(
+                    self.last_hop_channel.remote_pubkey)
                 self.output.print_line(
                     f"Calculating using capped fee rate {MAX_FEE_RATE} "
                     f"for inbound channel (with {last_hop_alias}, original fee rate {fee_rate})"
                 )
                 fee_rate = MAX_FEE_RATE
             policy = self.lnd.get_policy_to(self.last_hop_channel.chan_id)
-            fee_limit_msat = self.compute_fee(self.amount, self.fee_factor * fee_rate, policy) * 1_000
+            fee_limit_msat = self.compute_fee(
+                self.amount, self.fee_factor * fee_rate, policy) * 1_000
             fee_limit_msat = max(1_000, fee_limit_msat)
         else:
             return None
@@ -119,7 +131,8 @@ class Logic:
 
         fee_limit_formatted = format_fee_msat(int(fee_limit_msat))
         ppm_limit_formatted = format_ppm(ppm_limit)
-        self.output.print_without_linebreak(f"Setting fee limit to {fee_limit_formatted} ({ppm_limit_formatted})")
+        self.output.print_without_linebreak(
+            f"Setting fee limit to {fee_limit_formatted} ({ppm_limit_formatted})")
         if self.fee_factor != 1.0:
             self.output.print_line(f" (factor {self.fee_factor}).")
         else:
@@ -133,9 +146,11 @@ class Logic:
 
         tried_routes.append(route)
         self.output.print_line("")
-        self.output.print_without_linebreak(f"Trying route #{len(tried_routes)}")
+        self.output.print_without_linebreak(
+            f"Trying route #{len(tried_routes)}")
         fee_msat = route.total_fees_msat
-        route_ppm = int(route.total_fees_msat * 1_000_000 / route.total_amt_msat)
+        route_ppm = int(route.total_fees_msat *
+                        1_000_000 / route.total_amt_msat)
         fee_formatted = format_fee_msat(fee_msat)
         ppm_formatted = format_ppm(route_ppm)
         self.output.print_line(f" (fee {fee_formatted}, {ppm_formatted})")
@@ -185,22 +200,27 @@ class Logic:
             amount_msat / 1_000, policy_last_hop.fee_rate_milli_msat, policy_last_hop
         ) * 1_000
         difference_msat = -rebalance_fee_msat - missed_fee_msat + expected_income_msat
-        future_income_formatted = format_earning(math.floor(expected_income_msat), 8)
+        future_income_formatted = format_earning(
+            math.floor(expected_income_msat), 8)
         self.output.print_line(
             f"  {future_income_formatted}: "
             f"expected future fee income for inbound channel (with {last_hop_alias})"
         )
         missed_fee_formatted = format_fee_msat(math.ceil(missed_fee_msat), 8)
-        difference_formatted = format_fee_msat_white(math.ceil(difference_msat), 8)
-        transaction_fees_formatted = format_fee_msat(int(rebalance_fee_msat), 8)
-        self.output.print_line(f"- {transaction_fees_formatted}: rebalance transaction fees")
+        difference_formatted = format_fee_msat_white(
+            math.ceil(difference_msat), 8)
+        transaction_fees_formatted = format_fee_msat(
+            int(rebalance_fee_msat), 8)
+        self.output.print_line(
+            f"- {transaction_fees_formatted}: rebalance transaction fees")
         self.output.print_line(f"- {missed_fee_formatted}: "
                                f"missing out on future fees for outbound channel (with {first_hop_alias})")
         self.output.print_line(f"= {difference_formatted}: potential profit!")
 
     def handle_error(self, response, route, routes):
         code = response.failure.code
-        failure_source_pubkey = Logic.get_failure_source_pubkey(response, route)
+        failure_source_pubkey = Logic.get_failure_source_pubkey(
+            response, route)
         if code == 15:
             self.output.print_line(format_warning("Temporary channel failure"))
             routes.ignore_edge_on_route(failure_source_pubkey, route)
@@ -216,7 +236,8 @@ class Logic:
             self.output.print_line(format_warning("Incorrect CLTV expiry"))
             routes.ignore_edge_on_route(failure_source_pubkey, route)
         else:
-            self.output.print_line(format_error(f"Unknown error code {repr(code)}:"))
+            self.output.print_line(format_error(
+                f"Unknown error code {repr(code)}:"))
             self.output.print_line(format_error(repr(response)))
 
     @staticmethod
@@ -233,11 +254,14 @@ class Logic:
         first_hop = route.hops[0]
         last_hop = route.hops[-1]
         if self.low_outbound_liquidity_after_sending(first_hop, route.total_amt):
-            self.output.print_without_linebreak("Outbound channel would have low local ratio after sending, ")
-            routes.ignore_first_hop(self.get_channel_for_channel_id(first_hop.chan_id))
+            self.output.print_without_linebreak(
+                "Outbound channel would have low local ratio after sending, ")
+            routes.ignore_first_hop(
+                self.get_channel_for_channel_id(first_hop.chan_id))
             return True
         if self.first_hop_and_last_hop_use_same_channel(first_hop, last_hop):
-            self.output.print_without_linebreak("Outbound and inbound channel are identical, ")
+            self.output.print_without_linebreak(
+                "Outbound and inbound channel are identical, ")
             hop_before_last_hop = route.hops[-2]
             routes.ignore_edge_from_to(
                 last_hop.chan_id, hop_before_last_hop.pub_key, last_hop.pub_key
@@ -261,7 +285,8 @@ class Logic:
         channel_id = first_hop.chan_id
         channel = self.get_channel_for_channel_id(channel_id)
         if channel is None:
-            self.output.print_line(f"Unable to get channel information for hop {repr(first_hop)}")
+            self.output.print_line(
+                f"Unable to get channel information for hop {repr(first_hop)}")
             return True
 
         return max(0, channel.local_balance - channel.local_chan_reserve_sat) - total_amount < self.min_local
@@ -272,7 +297,8 @@ class Logic:
         channel_id = last_hop.chan_id
         channel = self.get_channel_for_channel_id(channel_id)
         if channel is None:
-            self.output.print_line(f"Unable to get channel information for hop {repr(last_hop)}")
+            self.output.print_line(
+                f"Unable to get channel information for hop {repr(last_hop)}")
             return True
 
         amount = last_hop.amt_to_forward
@@ -300,18 +326,22 @@ class Logic:
         high_fees = rebalance_fee_msat + missed_fee_msat > expected_income_msat
         if high_fees:
             if self.reckless:
-                self.output.print_line(format_error("Considering route with high fees"))
+                self.output.print_line(format_error(
+                    "Considering route with high fees"))
                 return False
             difference_msat = -rebalance_fee_msat - missed_fee_msat + expected_income_msat
-            first_hop_alias = format_alias(self.lnd.get_node_alias(route.hops[0].pub_key))
-            last_hop_alias = format_alias(self.lnd.get_node_alias(route.hops[-2].pub_key))
+            first_hop_alias = format_alias(
+                self.lnd.get_node_alias(route.hops[0].pub_key))
+            last_hop_alias = format_alias(
+                self.lnd.get_node_alias(route.hops[-2].pub_key))
             self.output.print_line("")
             if fee_rate_last_hop != original_fee_rate_last_hop:
                 self.output.print_line(
                     f"Calculating using capped fee rate {MAX_FEE_RATE} for inbound channel "
                     f"(with {last_hop_alias}, original fee rate {original_fee_rate_last_hop})"
                 )
-            route_ppm = int(route.total_fees_msat * 1_000_000 / route.total_amt_msat)
+            route_ppm = int(route.total_fees_msat *
+                            1_000_000 / route.total_amt_msat)
             route_fee_formatted = format_fee_msat(rebalance_fee_msat)
             route_ppm_formatted = format_ppm(route_ppm)
             self.output.print_line(
@@ -319,7 +349,8 @@ class Logic:
                 f"(fee {route_fee_formatted}, {route_ppm_formatted})"
             )
             self.output.print_route(route)
-            future_income_formatted = format_earning(math.floor(expected_income_msat), 8)
+            future_income_formatted = format_earning(
+                math.floor(expected_income_msat), 8)
             self.output.print_without_linebreak(
                 f"  {future_income_formatted}: "
                 f"expected future fee income for inbound channel (with {last_hop_alias})"
@@ -328,10 +359,14 @@ class Logic:
                 self.output.print_line(f" (factor {self.fee_factor})")
             else:
                 self.output.print_line("")
-            transaction_fees_formatted = format_fee_msat(int(rebalance_fee_msat), 8)
-            missed_fee_formatted = format_fee_msat(math.ceil(missed_fee_msat), 8)
-            difference_formatted = format_fee_msat_red(math.ceil(difference_msat), 8)
-            self.output.print_line(f"- {transaction_fees_formatted}: rebalance transaction fees")
+            transaction_fees_formatted = format_fee_msat(
+                int(rebalance_fee_msat), 8)
+            missed_fee_formatted = format_fee_msat(
+                math.ceil(missed_fee_msat), 8)
+            difference_formatted = format_fee_msat_red(
+                math.ceil(difference_msat), 8)
+            self.output.print_line(
+                f"- {transaction_fees_formatted}: rebalance transaction fees")
             self.output.print_line(f"- {missed_fee_formatted}: "
                                    f"missing out on future fees for outbound channel (with {first_hop_alias})")
             self.output.print_line(f"= {difference_formatted}")
@@ -347,6 +382,7 @@ class Logic:
         if self.last_hop_channel:
             memo = f"Rebalance of channel with ID {self.last_hop_channel.chan_id}"
         else:
+            print(self)
             memo = f"Rebalance of channel with ID {self.first_hop_channel.chan_id}"
         return self.lnd.generate_invoice(memo, self.amount)
 
@@ -365,13 +401,16 @@ class Logic:
 
     def initialize_ignored_channels(self, routes, fee_limit_msat, min_fee_last_hop):
         if self.reckless:
-            self.output.print_line(format_error("Also considering economically unviable channels for routes."))
+            self.output.print_line(format_error(
+                "Also considering economically unviable channels for routes."))
         for chan_id in self.excluded:
-            self.output.print_line(f"Channel {format_channel_id(chan_id)} is excluded:")
+            self.output.print_line(
+                f"Channel {format_channel_id(chan_id)} is excluded:")
             routes.ignore_channel(chan_id)
         if self.first_hop_channel:
             if min_fee_last_hop and not self.reckless:
-                self.ignore_cheap_channels_for_last_hop(min_fee_last_hop, routes)
+                self.ignore_cheap_channels_for_last_hop(
+                    min_fee_last_hop, routes)
             if not self.last_hop_channel and not self.reckless:
                 self.ignore_last_hops_with_low_inbound(routes)
 
@@ -384,7 +423,8 @@ class Logic:
             )
         if self.last_hop_channel:
             if not self.reckless:
-                self.ignore_first_hops_with_fee_rate_higher_than_last_hop(routes)
+                self.ignore_first_hops_with_fee_rate_higher_than_last_hop(
+                    routes)
             # avoid me - X - me via the same channel/peer
             chan_id = self.last_hop_channel.chan_id
             from_pub_key = self.lnd.get_own_pubkey()
@@ -394,7 +434,8 @@ class Logic:
             )
         if self.last_hop_channel and fee_limit_msat and not self.reckless:
             # ignore first hops with high fee rate configured by our node (causing high missed future fees)
-            max_fee_rate_first_hop = math.ceil(fee_limit_msat * 1_000 / self.amount)
+            max_fee_rate_first_hop = math.ceil(
+                fee_limit_msat * 1_000 / self.amount)
             for channel in self.lnd.get_channels():
                 fee_rate = self.lnd.get_ppm_to(channel.chan_id)
                 if fee_rate > max_fee_rate_first_hop and self.first_hop_channel != channel:

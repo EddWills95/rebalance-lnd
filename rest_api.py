12d74b5dbd686c24c62d2aad6b482aa42e0aa2d2
EddWills95 <edd.williams@me.com>
2021-10-05 20:33:22 +0100

    basically working implementation of rebalance

--- rest_api.py
+++ rest_api.py
@@ -1,3 +1,4 @@
+from argparse import Namespace
 from flask import Flask, request, render_template
 from flask import json
 from flask.json import jsonify
@@ -5,23 +6,30 @@ from output import Output, format_channel_id
 from rebalance import Rebalance, get_argument_parser, get_local_available, get_remote_available
 from dotenv import load_dotenv
 from flask_socketio import SocketIO, emit, send
-import jsonpickle
+from flask_cors import CORS
+import json
 import os
+import re
 
 load_dotenv()
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
 
 class WebSocketOutput(Output):
     @staticmethod
     def print_line(message, end='\n'):
-        send(message)
+        emit('rebalance', ansi_escape.sub('', message))
+        print(message)
 
     @staticmethod
     def print_without_linebreak(message):
-        send(message)
+        emit('rebalance', ansi_escape.sub('', message))
+        print(message)
 
 
 argument_parser = get_argument_parser()
@@ -35,20 +43,23 @@ rebalancer.ouput = WebSocketOutput(rebalancer.lnd)
 
 @socketio.on('connect')
 def test_connect(auth):
-    print('connected')
+    print('Connected', auth)
     send('Connected')
 
 
-@socketio.on('message')
-def message(message):
-    print(message)
-    if (message['type'] == 'rebalance'):
-
-        # Build Args
-        basic_arguments.to = message['to']
-        temp_rebalancer = Rebalance(basic_arguments)
-        temp_rebalancer.output = WebSocketOutput(temp_rebalancer.lnd)
-        temp_rebalancer.start()
+@socketio.on('rebalance')
+def message(incoming):
+    message = json.loads(incoming)
+    args = vars(basic_arguments)
+    # Build Args
+    args['list_candidates'] = False
+    args['to'] = message.get('to')
+    args['amount'] = message.get('amount')
+    args['first_hop_channel_id'] = message.get('from')
+    args['last_hop_channel_id'] = message.get('to')
+    temp_rebalancer = Rebalance(Namespace(**args))
+    temp_rebalancer.output = WebSocketOutput(temp_rebalancer.lnd)
+    temp_rebalancer.start()
 
 
 @app.get('/')
@@ -58,13 +69,16 @@ def index():
 
 @app.get('/channels')
 def channels():
-    # Check for incoming or outgoing.
+
+    args = vars(basic_arguments)
+
+    args['list_candidates'] = True
 
     # Collect channels and convert to nice JSON
-    channels = Rebalance(basic_arguments).list_channels()
+    channels = Rebalance(Namespace(**args)).start()
     return jsonify(list(map(lambda c:
                             {
-                                "channelId": c.chan_id,
+                                "channelId": str(c.chan_id),
                                 "alias": rebalancer.lnd.get_node_alias(c.remote_pubkey),
                                 "pubkey": c.remote_pubkey,
                                 "channelPoint": c.channel_point,
@@ -79,5 +93,7 @@ def channels():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    socketio.run(app, debug=True)
+    try:
+        socketio.run(app, debug=True)
+    except BaseException as base:
+        print('Preventing normal exit', base)
